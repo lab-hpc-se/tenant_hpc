326848f4bc0d7445e4354a0f58e0eae61944ca20
Dave Butland <dgbutland@gmail.com>
2024-09-07 15:37:46 +0100

    Initial terraform

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = var.region
+}
+
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
---
+++ terraform.tf
@@ -0,0 +1,42 @@
+# terraform {
+# }
+
+terraform {
+
+  #   backend "s3" {
+  #     bucket = "state"
+  #     key    = "tenant/hpc"
+  #     region = "us-east-1"
+  #   }
+
+  # cloud {
+  #   workspaces {
+  #     name = "learn-terraform-eks"
+  #   }
+  # }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.65.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6.1"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.5"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3.4"
+    }
+  }
+
+  required_version = "~> 1.9"
+}
+
